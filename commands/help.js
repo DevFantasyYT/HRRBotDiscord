4ecc14b2b1b1831300e5e8092d663f9cf6aec8c7
DevFantasyYT <60735010+DevFantasyYT@users.noreply.github.com>
2020-04-08 14:03:01 +0200

    update help menu

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, arguments) =>{
 
     try{
 
-        var text = "**__Commands__** \n\n `!whois` - Laat je profiel zien. \n `!serverinfo` - Laat de info van de server zien. \n `!botinfo` - Laat info van de bot zien. \n `!ticket` - Maak een ticket aan. \n\n **__Muziek commands__** \n\n `;;play` Doe dit commando met een link/naam van een nummer erachter en het nummer word afgespeelt. \n `;;skip` Skip een nummer. \n `;;join` Laat de bot joinen. \n `;;leave` Laat de bot leaven. \n `;;queue` Laat de queue zien. ";
+        var text = "**__Commands__** \n\n `!whois` - Laat je profiel zien. \n `!serverinfo` - Laat de info van de server zien. \n `!botinfo` - Laat info van de bot zien. \n\n **__Muziek commands__** \n\n `!play` Doe dit commando met een link/naam van een nummer erachter en het nummer word afgespeelt. \n `!skip` Skip een nummer. \n `!join` Laat de bot joinen. \n `;;leave` Laat de bot leaven. \n `!queue` Laat de queue zien. ";
         
         message.author.send(text);
 
